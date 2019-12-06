6e6aff6ac784d64f2006e66595b1ffea004f7a73
Rick Tastic <love@DESKTOP-CSGN4CO.attlocal.net>
2019-12-06 11:21:17 -0500

    Completed Task 1 - the Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    // define new elements
+    const newHeader = document.createElement('div'),
+    dateSpan = document.createElement('span'),
+    h1 = document.createElement('h1'),
+    tempSpan = document.createElement('span');
+
+    // set class names
+    newHeader.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    // set up structure of elements
+    newHeader.appendChild(dateSpan);
+    newHeader.appendChild(h1);
+    newHeader.appendChild(tempSpan);
+
+    // set text content
+    dateSpan.textContent = `SMARCH 28, 2019`;
+    h1.textContent = `Lambda Times`;
+    tempSpan.textContent = `98°`;
+
+    return newHeader;
+}
+
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
