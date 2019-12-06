40a9b882e951512a8ac326123eb273db24035235
Rick Tastic <love@DESKTOP-CSGN4CO.attlocal.net>
2019-12-06 13:06:30 -0500

    Completed 2nd and 3rd tasks

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        // console.log(response.data.articles);
+        const articles = Object.values(response.data.articles);
+        console.log(articles);
+        articles.forEach(array => {
+            array.forEach(article => {
+                let newCard = articleCreator(article);
+                articleEntryPoint.appendChild(newCard);
+            })
+        })
+    })
+
+
+function articleCreator(article) {
+    // define new elements
+    const newCard = document.createElement('div'),
+          headDiv = document.createElement('div'),
+          authDiv = document.createElement('div'),
+          imgDiv = document.createElement('div'),
+          authImg = document.createElement('img'),
+          authSpan = document.createElement('span');
+
+    // set class names
+    newCard.classList.add('card');
+    headDiv.classList.add('headline');
+    authDiv.classList.add('author');
+    imgDiv.classList.add('img-container');
+
+    // set up structure of the elements
+    newCard.appendChild(headDiv);
+    newCard.appendChild(authDiv);
+    authDiv.appendChild(imgDiv);
+    imgDiv.appendChild(authImg);
+    authDiv.appendChild(authSpan);
+
+    // set text content
+    headDiv.textContent = article.headline;
+    authImg.src = article.authorPhoto;
+    authSpan.textContent = `By ${article.authorName}`;
+
+    return newCard;
+}
+
+const articleEntryPoint = document.querySelector('.cards-container');
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        // console.log(response.data.topics);
+        response.data.topics.forEach(topic => {
+            let newTab = tabCreator(topic);
+            tabEntryPoint.appendChild(newTab);
+        })
+    })
+
+function tabCreator(topic) {
+    const newTab = document.createElement('div');
+    newTab.classList.add("tab");
+    newTab.textContent = topic;
+    return newTab;
+}
+
+const tabEntryPoint = document.querySelector('.topics');
